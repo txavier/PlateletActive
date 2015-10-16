72ab05d3f71a5df9a46708d97fb220b6bc07c689
txavier <txavier@gmail.com>
2015-10-16 19:57:22 -0400

    - Added windows service.

---
+++ PlateletActive.WindowsService/PlateletActiveService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+using System.Configuration;
+using PlateletActive.Core.Interfaces;
+
+namespace PlateletActive.WindowsService
+{
+    public partial class PlateletActiveService : ServiceBase
+    {
+        private static System.Timers.Timer maintenanceTimer;
+
+        private readonly IHplcDataService _hplcDataService;
+
+        public PlateletActiveService()
+        {
+            InitializeComponent();
+
+            // Initialize the log.
+            if (!System.Diagnostics.EventLog.SourceExists("PlateletActiveService"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "PlateletActiveService", "");
+            }
+            eventLog1.Source = "PlateletActiveService";
+
+            eventLog1.Log = "";
+
+            eventLog1.WriteEntry("PlateletActive Service initiating...");
+
+            var container = new StructureMap.Container(c => c.AddRegistry<PlateletActive.CompositionRoot.DefaultRegistry>());
+
+            _hplcDataService = container.GetInstance<IHplcDataService>();
+
+            // Initialize the maintenance timer.
+            int minutesTemp = 10;
+
+            var minutes = Int32.TryParse(ConfigurationManager.AppSettings["minutes"], out minutesTemp) ? minutesTemp : 60;
+
+            maintenanceTimer = new System.Timers.Timer(1000 * 60 * minutes);
+
+            maintenanceTimer.Elapsed += new System.Timers.ElapsedEventHandler(maintenanceTimer_Elapsed);
+
+            // Run maintenance every day.
+            maintenanceTimer.Interval = (1000 * 60 * minutes);
+
+            maintenanceTimer.Enabled = true;
+
+            eventLog1.WriteEntry("PlateletActive Service has initiated.");
+        }
+
+        void maintenanceTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                var inPath = ConfigurationManager.AppSettings["inPath"];
+
+                var outPath = ConfigurationManager.AppSettings["outPath"];
+
+                eventLog1.WriteEntry("PlateletActive import has begun.");
+
+                //// Get DEP Access Key
+                //string DepAccessKey = ConfigurationManager.AppSettings.Get("DepAccessKey");
+
+                //if (DepAccessKey == null)
+                //{
+                //    throw new Exception("Unable to continue the DEP Access key cannot be found.");
+                //}
+
+                _hplcDataService.ImportHplcData(inPath, outPath);
+
+                eventLog1.WriteEntry("PlateletActive import has ended.");
+            }
+            catch (Exception ex)
+            {
+                eventLog1.WriteEntry("PlateletActiveService system maintenance has thrown an exception. " + GetStackTrace(ex));
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                int minutesTemp = 0;
+
+                var minutes = Int32.TryParse(ConfigurationManager.AppSettings["minutes"], out minutesTemp) ? minutesTemp : 60;
+
+                maintenanceTimer.Interval = (1000 * 60 * minutes);
+
+                maintenanceTimer.Enabled = true;
+
+                eventLog1.WriteEntry("Platelet Active Service started");
+            }
+            catch (Exception ex)
+            {
+                eventLog1.WriteEntry(GetStackTrace(ex));
+            }
+        }
+
+        public string GetStackTrace(Exception ex)
+        {
+            if (ex.InnerException != null)
+            {
+                return ex.ToString() + "--------------------------" +
+                    GetStackTrace(ex.InnerException);
+            }
+
+            return ex.ToString();
+        }
+
+        protected override void OnStop()
+        {
+            maintenanceTimer.Enabled = false;
+
+            eventLog1.WriteEntry("Platelet Active Service stopped");
+        }
+    }
+}
---
+++ PlateletActive.WindowsService/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateletActive.WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    // Run your service normally.
+                    ServiceBase[] ServicesToRun = new ServiceBase[] { new PlateletActiveService() };
+                    ServiceBase.Run(ServicesToRun);
+                }
+                else if (args.Length == 1)
+                {
+                    switch (args[0])
+                    {
+                        case "-install":
+                            InstallService();
+                            StartService();
+                            break;
+                        case "-uninstall":
+                            StopService();
+                            UninstallService();
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+
+                //if (Environment.UserInteractive)
+                //{
+                //    string parameter = string.Concat(args);
+                //    switch (parameter)
+                //    {
+                //        case "--install":
+                //            //System.Configuration.Install.ManagedInstallerClass.InstallHelper(new[] { System.Reflection.Assembly.GetExecutingAssembly().Location });
+                //            InstallService();
+                //            StartService();
+                //            break;
+                //        case "--uninstall":
+                //            //System.Configuration.Install.ManagedInstallerClass.InstallHelper(new[] { "/u", System.Reflection.Assembly.GetExecutingAssembly().Location });
+                //            StopService();
+                //            UninstallService();
+                //            break;
+                //    }
+                //}
+                //else
+                //{
+
+                //    ServiceBase[] ServicesToRun;
+
+                //    ServicesToRun = new ServiceBase[]
+                //        {
+                //        new PlateletActiveService()
+                //        };
+
+                //    ServiceBase.Run(ServicesToRun);
+                //}
+
+                ////ServiceBase[] ServicesToRun;
+                ////ServicesToRun = new ServiceBase[]
+                ////{
+                ////    new PlateletActiveService()
+                ////};
+                ////ServiceBase.Run(ServicesToRun);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static void InstallService()
+        {
+            if (IsInstalled()) return;
+
+            try
+            {
+                using (System.Configuration.Install.AssemblyInstaller installer = GetInstaller())
+                {
+                    IDictionary state = new Hashtable();
+                    try
+                    {
+                        installer.Install(state);
+                        installer.Commit(state);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            installer.Rollback(state);
+                        }
+                        catch { }
+                        throw;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void UninstallService()
+        {
+            if (!IsInstalled()) return;
+            try
+            {
+                using (System.Configuration.Install.AssemblyInstaller installer = GetInstaller())
+                {
+                    IDictionary state = new Hashtable();
+                    try
+                    {
+                        installer.Uninstall(state);
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void StartService()
+        {
+            if (!IsInstalled()) return;
+
+            using (ServiceController controller =
+                new ServiceController("PlateletActiveService"))
+            {
+                try
+                {
+                    if (controller.Status != ServiceControllerStatus.Running)
+                    {
+                        controller.Start();
+                        controller.WaitForStatus(ServiceControllerStatus.Running,
+                            TimeSpan.FromSeconds(10));
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static void StopService()
+        {
+            if (!IsInstalled()) return;
+            using (ServiceController controller =
+                new ServiceController("PlateletActiveService"))
+            {
+                try
+                {
+                    if (controller.Status != ServiceControllerStatus.Stopped)
+                    {
+                        controller.Stop();
+                        controller.WaitForStatus(ServiceControllerStatus.Stopped,
+                             TimeSpan.FromSeconds(10));
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static bool IsInstalled()
+        {
+            using (ServiceController controller =
+                new ServiceController("PlateletActiveService"))
+            {
+                try
+                {
+                    ServiceControllerStatus status = controller.Status;
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        private static bool IsRunning()
+        {
+            using (ServiceController controller =
+                new ServiceController("PlateletActiveService"))
+            {
+                if (!IsInstalled()) return false;
+                return (controller.Status == ServiceControllerStatus.Running);
+            }
+        }
+
+        private static System.Configuration.Install.AssemblyInstaller GetInstaller()
+        {
+            System.Configuration.Install.AssemblyInstaller installer = new System.Configuration.Install.AssemblyInstaller(
+                typeof(PlateletActiveService).Assembly, null);
+            installer.UseNewContext = true;
+            return installer;
+        }
+
+    }
+}
